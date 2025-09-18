bcdd1f1005ef4b2e232069c920f550894cbcc600
wwz09 <wwz5124@gmail.com>
2025-09-18 18:34:30 +0800

    del

--- uci-defaults/91_info.sh
+++
@@ -1,4 +0,0 @@
-uci del dropbear.main.enable
-uci del dropbear.main.Interface
-uci del dropbear.main.RootPasswordAuth
-uci commit dropbear
--- uci-defaults/94_source.sh
+++
@@ -1,1 +0,0 @@
-sed -e 's,https://downloads.immortalwrt.org,https://mirror.nju.edu.cn/immortalwrt,g' -e 's,https://mirrors.vsean.net/openwrt,https://mirror.nju.edu.cn/immortalwrt,g' -i.bak /etc/opkg/distfeeds.conf
